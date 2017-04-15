d5d89b260c722e0d5ac55c8e16f377b005b5f9f4
Bio Hazard <hazardland@gmail.com>
2017-04-15 11:15:45 +0400

    list + sync commands

--- module.php
+++ module.php
@@ -3,22 +3,100 @@
 	require __DIR__.'/lib/console/console.php';
 	require __DIR__.'/lib/debug/debug.php';
 
-	$home = getcwd();
-	if (!file_exists($home.'/modules.php'))
+	function execute ($command)
 	{
-		echo color("Please cd to into your /app folder", RED);
+		$output = [];
+		exec ($command, $output);
+		return implode("\n", $output);
+	}
+
+	$home = getcwd();
+	if (!file_exists($home.'/.modules'))
+	{
+		echo color("Please cd to into folder containing file .modules", RED);
 		exit;
 	}
 
-	$modules = include $home.'/modules.php';
 
-	//debug ($argv);
-	//debug ($modules)
+	/*
+	 * collect module data
+	 */
+	$modules = [];
+	if ($file = fopen($home.'/.modules', 'r'))
+	{
+	    while(!feof($file))
+	    {
+	        $line = trim(fgets($file));
+	        if ($line!='')
+	        {
+	        	$line = explode (' ', $line);
+	        	if (is_array($line))
+	        	{
+	        		$path = $line[0];
+	        		if (!isset($line[1]))
+	        		{
+	        			$name = null;
+	        		}
+	        		else
+	        		{
+	        			$name = $line[1];
+	        		}
+	        		$modules [$path] = $name;
+	        	}
+	        }
+	    }
+	    fclose($file);
+	}
+	else
+	{
+		echo color("Error opening .modules file", RED);
+	}
 
+
+	/**
+	 * process command
+	 */
 	if (isset($argv[1]))
 	{
 		$command = strtolower($argv[1]);
-		if ($command==='sync')
+		if ($command=='list')
+		{
+			foreach ($modules as $path => $name)
+			{
+				if ($path==='/' && $name===null)
+				{
+					$name = '*';
+				}
+				if ($name!==null)
+				{
+					echo color ($name,YELLOW)." ";
+				}
+				if ($name!=$path)
+				{
+					echo color($path,CYAN);
+				}
+				if (file_exists($home.'/'.$path) && chdir($home.'/'.$path))
+				{
+					$result = execute("git status");
+					$changes = false;
+					if (strpos($result,'nothing to commit')===false)
+					{
+						$modified = substr_count($result, 'modified:');
+						if (!$modified)
+						{
+							$modified = '';
+						}
+						echo " ".color("+".$modified,RED);
+					}
+					echo "\n";
+				}
+				else
+				{
+					echo " ".color("[bad path ".$path."]", RED)."\n";
+				}
+			}
+		}
+		else if ($command==='sync')
 		{
 			if (!isset($argv[2]) || !$argv[2])
 			{
@@ -26,12 +104,12 @@
 				exit;
 			}
 			$commit = $argv[2];
-			if (!isset($modules) || !is_array($modules))
+			if (!$modules)
 			{
-				echo color ("Module list not found please check modules.php", RED);
+				echo color ("Module list empty", RED);
 				exit;
 			}
-			foreach ($modules as $name => $path)
+			foreach ($modules as $path => $name)
 			{
 				if (!file_exists($home.'/'.$path))
 				{
@@ -39,26 +117,34 @@
 					exit;
 				}
 			}
-			foreach ($modules as $name => $path)
+			foreach ($modules as $path => $name)
 			{
 				if (chdir($home.'/'.$path))
 				{
-					echo color("\nSyncing",GREEN)." ".color($name,YELLOW)."\n";
-					$result = strtolower(exec("git status"));
+					if ($path==='/' && $name===null)
+					{
+						$name = '*';
+					}
+					if ($name===null)
+					{
+						$name = $path;
+					}
+					echo color("\nSyncing",GREEN)." ".color($name,PINK)."\n";
+					$result = strtolower(execute("git status"));
 					$changes = false;
 					if (strpos($result,'nothing to commit')===false)
 					{
 						$changes = true;
 						echo color("Commiting",BLUE)."\n";
-						echo exec ("git add --all")."\n";
-						echo exec ("git commit -a -m \"".$commit."\"")."\n";
+						echo execute ("git add --all")."\n";
+						echo execute ("git commit -a -m \"".$commit."\"")."\n";
 					}
 					echo color("Pulling",BLUE)."\n";
-					echo exec ("git pull origin master")."\n";
+					echo execute ("git pull origin master")."\n";
 					if ($changes)
 					{
 						echo color("Pushing",BLUE)."\n";
-						echo exec ("git push origin master")."\n";
+						echo execute ("git push origin master")."\n";
 					}
 				}
 				else
