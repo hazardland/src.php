bff98dbff7f72e94427e5fa19caccdb63b1402e5
Bio Hazard <hazardland@gmail.com>
2017-04-17 16:28:39 +0400

    retrucktored

---
+++ app/lib.php
@@ -0,0 +1,72 @@
+<?php
+
+function execute ($command, $debug=false, $color=BLUE)
+{
+	$output = [];
+	if ($debug)
+	{
+		echo color ($command, $color)."\n";
+	}
+	exec ($command, $output);
+	return implode("\n", $output);
+}
+
+function config ($file)
+{
+	$content = trim(file_get_contents($file));
+	//debug ($content,'content');
+	$content = preg_replace(['/[\s]+\n/', '/\n{1,}+/', '/\#.+\n/'], ["\n","\n",''], $content);
+	return $content;
+}
+
+function repo_load ($link)
+{
+	$content = config ($link);
+	if ($content)
+	{
+		$items = explode("\n",$content);
+		foreach ($items as $item)
+		{
+			$repo = explode('=',$item);
+			if (count($repo)>1)
+			{
+				$data = explode(' ',$repo[1]);
+				$link = $data[0];
+				if (count($data)>1)
+				{
+					$branch = $data[1];
+				}
+				else
+				{
+					$branch = 'master';
+				}
+				app::$repos[$repo[0]]['link'] = $link;
+				app::$repos[$repo[0]]['branch'] = $branch;
+			}
+		}
+	}
+}
+function repo_cache ($path)
+{
+	if (is_array(app::$repos) && count(app::$repos))
+	{
+		$result = '';
+		foreach (app::$repos as $name => $repo)
+		{
+			$result .= $name.'='.$repo['link'].' '.$repo['branch']."\n";
+		}
+		file_put_contents($path, $result);
+	}
+}
+function modules_save ($path)
+{
+	if (is_array(app::$modules) && count(app::$modules))
+	{
+		$result = '';
+		foreach (app::$modules as $name => $module)
+		{
+			$result .= $module->line()."\n";
+		}
+		file_put_contents($path, $result);
+	}
+}
